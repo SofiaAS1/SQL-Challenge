eb07a8986a6482e2a9771f52185bec8e4ce03337
SAS 826 <sasotillo826@gmail.com>
2020-08-03 23:10:24 -0400

    Updated sql dbs code with foreign keys

--- EmployeeSQL/EmployeeSQL.sql
+++ EmployeeSQL/EmployeeSQL.sql
@@ -10,8 +10,10 @@ select * from departments;
 Drop table if exists dept_emp;
 
 create table dept_emp (
-	emp_no varchar(81),
-	dept_no varchar(81)
+	emp_no int,
+	dept_no varchar(81),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
 select * from dept_emp;
@@ -20,7 +22,9 @@ Drop table if exists dept_manager;
 
 create table dept_manager (
 	dept_no varchar(81),
-	emp_no varchar(81)
+	emp_no int,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
 select * from dept_manager;
@@ -28,13 +32,14 @@ select * from dept_manager;
 Drop table if exists employees;
 
 create table employees (
-	emp_no serial primary key,
+	emp_no int primary key,
 	emp_title_id varchar(81),
 	birth_date date not null,
 	first_name varchar(81),
 	last_name varchar(81),
 	sex varchar(81),
-	hire_date date not null
+	hire_date date not null,
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
 );
 
 select * from employees;
@@ -42,8 +47,9 @@ select * from employees;
 Drop table if exists salaries;
 
 create table salaries (
-	emp_no serial primary key,
-	salary int
+	emp_no serial,
+	salary int,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
 select * from salaries;
@@ -51,7 +57,7 @@ select * from salaries;
 Drop table if exists titles;
 
 create table titles (
-	title_id varchar(81),
+	title_id varchar(81) primary key,
 	title varchar(81)
 );
 
